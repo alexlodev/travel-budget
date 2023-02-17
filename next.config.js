301159c7bd23038d8011fde6f0ffeb3e3ff90190
Alexis Lo <alexislo15@hotmail.es>
2023-02-16 18:21:41 -0600

    fix pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-})
+const withPWA = require('next-pwa')()
 
 module.exports = withPWA({
-  // config
-})
+  dest: 'public',
+  register: true,
+  skipWaiting: true
+});
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/192.png" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Public+Sans&display=swap"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
